2bf3f71a0ab11d333c4b48b1a7a8d717c086b5fe
u1169 <u1169@ijuzhong.com>
2021-01-09 17:46:32 +0800

    .git

---
+++ vendor/vladimir-yuldashev/laravel-queue-rabbitmq/src/VladimirYuldashev/LaravelQueueRabbitMQ/LaravelQueueRabbitMQServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace VladimirYuldashev\LaravelQueueRabbitMQ;
+
+use Illuminate\Queue\QueueManager;
+use Illuminate\Support\ServiceProvider;
+use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Connectors\RabbitMQConnector;
+
+class LaravelQueueRabbitMQServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/rabbitmq.php', 'queue.connections.rabbitmq'
+        );
+    }
+
+    /**
+     * Register the application's event listeners.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /** @var QueueManager $queue */
+        $queue = $this->app['queue'];
+        $connector = new RabbitMQConnector;
+
+        $queue->stopping(function () use ($connector) {
+            $connector->connection()->close();
+        });
+
+        $queue->addConnector('rabbitmq', function () use ($connector) {
+            return $connector;
+        });
+    }
+}
---
+++ vendor/vladimir-yuldashev/laravel-queue-rabbitmq/src/VladimirYuldashev/LaravelQueueRabbitMQ/Queue/Jobs/RabbitMQJob.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs;
+
+use Exception;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Queue\Job as JobContract;
+use Illuminate\Database\DetectsDeadlocks;
+use Illuminate\Queue\Jobs\Job;
+use Illuminate\Support\Str;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Message\AMQPMessage;
+use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue;
+
+class RabbitMQJob extends Job implements JobContract
+{
+    use DetectsDeadlocks;
+
+    /**
+     * Same as RabbitMQQueue, used for attempt counts.
+     */
+    const ATTEMPT_COUNT_HEADERS_KEY = 'attempts_count';
+
+    protected $connection;
+    protected $channel;
+    protected $queue;
+    protected $message;
+
+    /**
+     * Creates a new instance of RabbitMQJob.
+     *
+     * @param \Illuminate\Container\Container $container
+     * @param \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $connection
+     * @param \PhpAmqpLib\Channel\AMQPChannel $channel
+     * @param string $queue
+     * @param \PhpAmqpLib\Message\AMQPMessage $message
+     */
+    public function __construct(
+        Container $container,
+        RabbitMQQueue $connection,
+        AMQPChannel $channel,
+        $queue,
+        AMQPMessage $message
+    ) {
+        $this->container = $container;
+        $this->connection = $connection;
+        $this->channel = $channel;
+        $this->queue = $queue;
+        $this->message = $message;
+    }
+
+    /**
+     * Fire the job.
+     * @return void
+     * @throws Exception
+     */
+    public function fire()
+    {
+        $payload = $this->payload();
+
+        list($class, $method) = $this->parseJob($payload['job']);
+
+        $this->instance = $this->resolve($class);
+
+        try {
+            $this->instance->{$method}($this, $payload['data']);
+        } catch (Exception $exception) {
+            if (
+                $this->causedByDeadlock($exception) ||
+                Str::contains($exception->getMessage(), ['detected deadlock'])
+            ) {
+                sleep(2);
+                $this->fire();
+                return;
+            }
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * Get the number of times the job has been attempted.
+     *
+     * @return int
+     */
+    public function attempts()
+    {
+        if ($this->message->has('application_headers') === true) {
+            $headers = $this->message->get('application_headers')->getNativeData();
+
+            if (isset($headers[self::ATTEMPT_COUNT_HEADERS_KEY]) === true) {
+                return $headers[self::ATTEMPT_COUNT_HEADERS_KEY];
+            }
+        }
+
+        // set default job attempts to 1 so that jobs can run without retry
+        return 1;
+    }
+
+    /**
+     * Get the raw body string for the job.
+     *
+     * @return string
+     */
+    public function getRawBody()
+    {
+        return $this->message->body;
+    }
+
+    /**
+     * Delete the job from the queue.
+     *
+     * @return void
+     */
+    public function delete()
+    {
+        parent::delete();
+        $this->channel->basic_ack($this->message->get('delivery_tag'));
+    }
+
+    /**
+     * Release the job back into the queue.
+     *
+     * @param int $delay
+     * @return void
+     * @throws Exception
+     */
+    public function release($delay = 0)
+    {
+        parent::release($delay);
+
+        $this->delete();
+        $this->setAttempts($this->attempts() + 1);
+
+        $body = $this->payload();
+
+        /*
+         * Some jobs don't have the command set, so fall back to just sending it the job name string
+         */
+        if (isset($body['data']['command']) === true) {
+            $job = $this->unserialize($body);
+        } else {
+            $job = $this->getName();
+        }
+
+        $data = $body['data'];
+
+        if ($delay > 0) {
+            $this->connection->later($delay, $job, $data, $this->getQueue());
+        } else {
+            $this->connection->push($job, $data, $this->getQueue());
+        }
+    }
+
+    /**
+     * Sets the count of attempts at processing this job.
+     *
+     * @param int $count
+     *
+     * @return void
+     */
+    private function setAttempts($count)
+    {
+        $this->connection->setAttempts($count);
+    }
+
+    /**
+     * Get the job identifier.
+     *
+     * @return string
+     */
+    public function getJobId()
+    {
+        return $this->message->get('correlation_id');
+    }
+
+    /**
+     * Sets the job identifier.
+     *
+     * @param string $id
+     *
+     * @return void
+     */
+    public function setJobId($id)
+    {
+        $this->connection->setCorrelationId($id);
+    }
+
+    /**
+     * Unserialize job
+     *
+     * @param array $body
+     * @return mixed
+     * @throws Exception
+     */
+    private function unserialize(array $body)
+    {
+        try {
+            return unserialize($body['data']['command']);
+        } catch (Exception $exception) {
+            if (
+                $this->causedByDeadlock($exception) ||
+                Str::contains($exception->getMessage(), ['detected deadlock'])
+            ) {
+                sleep(2);
+                return $this->unserialize($body);
+            }
+
+            throw $exception;
+        }
+    }
+}
